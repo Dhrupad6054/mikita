b74875c7da861f310db1fd34342196d5e3147a21
Dhrupad6054 <mahimiku15@gmail.com>
2022-05-29 22:37:08 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 function App() {
-  const myName = "mikita";
-  console.log(myName);
   return (
     <div className="App">
-      <h1>Welcome to my world and my name is {myName}</h1>
+      <h1>hello my name is mikita</h1>
     </div>
   );
 }
+// function App() {
+//   const myName = "mikita";
+//   console.log(myName);
+//   return (
+//     <div className="App">
+//       <h1>Welcome to my world and my name is {myName}</h1>
+//     </div>
+//   );
+// }
 export default App;
